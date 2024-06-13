ac1f81f051f21e1cc8c02e16d502f17986d819f1
vietnamican <vietnamican@gmail.com>
2024-06-13 16:18:02 +0700

    change logic params

--- analysis.js
+++ analysis.js
@@ -43,7 +43,8 @@ async function statistic() {
     // const starttimeFormatted = "2024-06-02T10:24:00+07:00";
     // const starttimeFormatted = "2024-06-05T11:15+07:00";
     // const starttimeFormatted = "2024-06-05T21:30+07:00";
-    const starttimeFormatted = "2024-06-10T11:44+07:00";
+    // const starttimeFormatted = "2024-06-10T11:44+07:00";
+    const starttimeFormatted = "2024-06-13T14:17+07:00";
     const starttime = convertToEpochTime(starttimeFormatted);
     console.log(starttime);
     // const starttime = 1716045120000
